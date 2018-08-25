c8baa9f67c0c33a3d39d4397d874ba099c7f4ed2
Tomas <tcarneado@gmail.com>
2018-08-25 09:31:30 +0200

    from api/values to api/primes

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -8,35 +8,35 @@ namespace primeapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PrimesController : ControllerBase
     {
-        // GET api/values
+        // GET api/primes
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/primes/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/primes
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/primes/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/primes/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
