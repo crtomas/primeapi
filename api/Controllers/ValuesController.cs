b5f5604e9f096146db6dd61d72cd6edac47d21a1
Tomas <tcarneado@gmail.com>
2018-08-25 13:59:59 +0200

    17,19

--- api/Controllers/ValuesController.cs
+++ api/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace primeapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "2", "3","5","7","11","13","..." };
+            return new string[] { "2", "3","5","7","11","13","17","19","..." };
         }
 
         // GET api/primes/5
